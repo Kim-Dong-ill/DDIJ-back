b1bb25c787654a6342b5f3e2547be982765fe9be
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-05-07 18:03:52 +0900

    basic

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+dotenv.config();
+
+const server = async function () {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("db connected");
+
+    mongoose.set("debug", true);
+    app.use(express.json());
+
+    app.listen(4000);
+  } catch (error) {
+    console.log("no db");
+  }
+};
+
+server();
