f2193fce69c9ac9439073dffb1e021bd88788c94
ked1125 <ked1125@naver.com>
2024-05-22 19:00:59 +0900

    240522_back_patch라우터수정

--- src/routers/petRouter.js
+++ src/routers/petRouter.js
@@ -12,18 +12,6 @@ petRouter.get("/list/:userId", async (req, res) => {
   }
 });
 
-// petRouter.put("/:petId", async (req, res) => {
-//   try {
-//     let { petId } = req.params;
-//     const temp = {
-//       message: "마이펫이지-펫 정보 수정하기.",
-//     };
-//     return res.status(200).send(temp);
-//   } catch (error) {
-//     res.status(500).send(error.message);
-//   }
-// });
-
 // petRouter.post("/modimg/:petId", async (req, res) => {
 //   try {
 //     let { petId } = req.params;
@@ -93,4 +81,38 @@ petRouter.get("/pet/list/:userId", async (req, res) => {
   }
 });
 
+// 강아지 index값 수정하려고 만든 api -> 대표강아지 설정시 이용합니다.
+petRouter.patch("/mainpetindex", async (req, res) => {
+  try {
+    const { petId1, petId2 } = req.body; // 요청 본문에서 두 개의 petId를 가져옴
+
+    // 두 개의 petId를 사용해 각각의 펫을 찾음
+    const pet1 = await Pet.findById(petId1);
+    const pet2 = await Pet.findById(petId2);
+
+    // 각각의 펫이 존재하는지 확인
+    if (!pet1 || !pet2) {
+      return res.status(404).send("One or both pets not found");
+    }
+
+    // 두 펫의 index 값을 서로 맞바꿈
+    const tempIndex = pet1.index;
+    pet1.index = pet2.index;
+    pet2.index = tempIndex;
+
+    // 변경된 값을 저장
+    await pet1.save();
+    await pet2.save();
+
+    // 업데이트된 펫 정보를 응답
+    return res.status(200).send({ pet1, pet2 });
+  } catch (error) {
+    res.status(500).send(error.message); // 오류 발생 시 500 에러 응답
+  }
+});
+
 module.exports = petRouter;
+
+module.exports = petRouter;
+
+// 유저 유저수정 펫리스트보기 펫리스트수정 제외하고 나머지..?
